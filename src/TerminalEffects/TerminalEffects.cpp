e48473940f38167da3f978ac1978f7eb641cec6e
mrange <marten_range@hotmail.com>
2024-12-29 14:28:53 +0100

    GG

--- src/TerminalEffects/TerminalEffects.cpp
+++ src/TerminalEffects/TerminalEffects.cpp
@@ -343,7 +343,6 @@ namespace {
  ▒ ░░      ░   ░░        ░░░ ░ ░   ░ ░   ░  ░  ░     ░       ░
  ░         ░               ░         ░  ░      ░     ░  ░ ░   
 
-                 ☁ ☁ ☁  G E R P - 2 0 2 5 ☁ ☁ ☁
                                      ╭─────╮
                ╭─────────────────────┤▄▀▄▀▄├───────────────╮
    ┼───────────┼ ▀ G L I M G L A M ▄ │▄▀▄▀▄│ ▄ L A N C E ▀ │
@@ -351,6 +350,30 @@ namespace {
    ╰───────────┼          │ ▀ L O N G S H O T ▄ │
                           ╘═════════════════════╛
 )BITMAP");
+
+  bitmap gerp = make_bitmap(col__black, LR"BITMAP(
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░      ░░░░░░░░░        ░░░░░░░░       ░░░░░░░░░       ░░░░░░░░
+▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒▒▒▒▒▒
+▓▓▓▓▓▓▓  ▓▓▓   ▓▓▓▓▓▓▓▓      ▓▓▓▓▓▓▓▓▓▓       ▓▓▓▓▓▓▓▓▓       ▓▓▓▓▓▓▓▓
+███████  ████  ████████  ██████████████  ███  █████████  █████████████
+████████      █████████        ████████  ████  ████████  █████████████
+██████████████████████████████████████████████████████████████████████
+
+      
+         ╭─────────────────────────────────────────────────╮
+         │     .         .        ♥       *             .  │
+         │ *       ████     ████     ████    ██████     .  │
+  ╭────╮ │   *    █░░░ █   █░░░██   █░░░ █  ░█░░░░         │ ╭────╮ 
+  ╱ ◢◣ ╲ │       ░    ░█  ░█  █░█  ░    ░█  ░█████   *     │ ╱ ◢◣ ╲ 
+ ╱━━━━━━╲├────━━════███════█ █ ░█═════███═════════█═━━─────┤╱━━━━━━╲
+ ╰○○○○○○╯│      *  █░░    ░██  ░█    █░░         ░█      . │╰○○○○○○╯
+         │  .     █       ░█   ░█   █        █   ░█   . *  │
+         │       ░██████  ░ ████   ░██████  ░ ████         │
+         │.  ♥   ░░░░░░    ░░░░    ░░░░░░    ░░░░   ♥      │
+         │              *        .        *                │
+         ╘═════════════════════════════════════════════════╛
+)BITMAP");          
 
   bitmap sixel_pixel = make_bitmap(col__rainbow, LR"BITMAP(
   ████████ ██                  ██   ███████  ██                  ██
@@ -589,6 +612,59 @@ namespace {
         auto col0 = palette(d+time+py);
         auto col1 = palette(d+1.5F+time*0.707F+py);
         auto col = d < 0.0 ? col0 : col1;
+        col *= smoothstep(-0.5F, 0.5F, -std::cosf(time-py-0.5F*px*px));
+        screen.draw__pixel(
+            L' '
+          , vec3{0,0,0}
+          , col
+          , x
+          , y
+          );
+      }
+    }
+    screen.draw__bitmap(impulse2  , time, 8, 6);
+//    screen.draw__bitmap(gerp        , time, 5, 6);
+
+  }
+
+  void effect2(float time, screen & screen) {
+    auto df = [](float x, float y) -> float {
+      const float m = 0.5;
+      float l = length(x,y);
+      l = std::fmodf(l+(0.5F*m),m)-(0.5F*m);
+      return std::abs(l)-(m*0.25F);
+    };
+
+    for (std::size_t y = 0; y < screen.height; ++y) {
+      auto py = (-1.F*screen.height+2.F*y)/screen.height;
+      for (std::size_t x = 0; x < screen.width; ++x) {
+        auto px = (-1.F*screen.width+2.F*x )/screen.width;
+
+        auto px0 = px;
+        auto py0 = py;
+
+        px0 -= std::sinf(0.707f*(time+100));
+        py0 -= std::sinf((time+100));
+
+        auto px1 = px;
+        auto py1 = py;
+
+        px1 -= std::sinf(0.5F*(time+123));
+        py1 -= std::sinf(0.707F*(time+123));
+        float sm = 0.125F*length(px, py);
+
+        auto d0 = df(px0, py0);
+        auto d1 = df(px1, py1);
+        auto d  = d0;
+        d = pmax(d, d1, sm);
+        float dd = -d0;
+        dd = pmax(dd, -d1, sm);
+        d =  std::min(d, dd);
+
+        auto col0 = palette(d+time+py);
+        auto col1 = palette(d+1.5F+time*0.707F+py);
+        auto col = d < 0.0 ? col0 : col1;
+        col *= smoothstep(-0.5F, 0.5F, std::sinf(time-py-0.5F*px*px));
         screen.draw__pixel(
             L' '
           , vec3{0,0,0}
